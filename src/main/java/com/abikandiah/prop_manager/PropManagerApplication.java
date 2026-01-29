9694795d207d5ffeaca3c790c1f094b141b4db10
abi <abikandiah@gmail.com>
2026-01-29 16:27:01 -0500

    feat: Initial project setup (license, gitignore)

---
+++ src/main/java/com/abikandiah/prop_manager/PropManagerApplication.java
@@ -0,0 +1,13 @@
+package com.abikandiah.prop_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropManagerApplication.class, args);
+	}
+
+}
